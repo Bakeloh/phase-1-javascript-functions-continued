5f58f95b5d629e3aa5adef85a5232c04e54a7988
Bakeloh <bakelohnoel@gmail.com>
2023-10-19 13:19:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(activity = "This Saturday,", target = "roller-skate!") {
+    return `${activity} I want to ${target}`;
+}
+console.log
+
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(obj = "*") {
+    if (obj === "||") {
+        return function (adjective = "a dedicated programmer") { return `You are ${obj}${adjective}${obj}!` };
+    }
+    else {
+        return function (adjective = "a hard worker") { return `You are ${obj}${adjective}${obj}!` };
+    }
+};
+
+wrapAdjective();
+
+
